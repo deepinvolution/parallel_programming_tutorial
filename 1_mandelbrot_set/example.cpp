5db60065c195dc441c294549870a5b5dacfc1997
wenchuanchen <a0956272621@gmail.com>
2023-12-06 23:32:26 +0800

    update

--- 1_mandelbrot_set/example.cpp
+++ 1_mandelbrot_set/example.cpp
@@ -1,7 +1,18 @@
 #include "utils.hpp"
+#include <vector>
 
 int main(int argc, char** argv) {
-    int x0 = 0, x1 = 1, y0 = 0, y1 = 1, w = 10, h = 10;
-    mandelbrot_set(0.1, 0.2);
+    float x0 = 0.0, x1 = 2.0, y0 = 0.0, y1 = 2.0, m = 10.0, n = 10.0;
+    float gx = (x1 - x0) / m, gy = (y1 - y0) / n;
+    std::vector<std::vector<int>> plot(m, std::vector<int>(n));
+    for (int i = 0; i < m; i++)
+        for (int j = 0; j < n; j++)
+            plot[i][j] = mandelbrot_set(x0 + gx * i, y0 + gy * j);
+    std::cout << "plot:" << std::endl;
+    for (int i = 0; i < m; i++) {
+        for (int j = 0; j < n; j++)
+            std::cout << plot[i][j] << " ";
+        std::cout << std::endl;
+    }
     return 0;
 }
--- 1_mandelbrot_set/utils.hpp
+++ 1_mandelbrot_set/utils.hpp
@@ -1,18 +1,21 @@
 #include <iostream>
 
+#define INF 10000
+
 template <typename T>
 class Complex;
 template <typename T>
 std::ostream& operator<<(std::ostream& os, Complex<T>& rhs);
 template <typename T>
-Complex<T>& operator*(Complex<T>& lhs, Complex<T>& rhs);
+Complex<T> operator*(Complex<T>& lhs, Complex<T>& rhs);
 
 template <typename T>
 class Complex {
 public:
     Complex(T real, T img) : real(real), img(img) {}
     friend std::ostream& operator<<<T>(std::ostream& os, Complex<T>& rhs);
-    friend Complex<T>& operator*<T>(Complex<T>& lhs, Complex<T>& rhs);
+    friend Complex<T> operator*<T>(Complex<T>& lhs, Complex<T>& rhs);
+    float size() { return real * real + img * img; }
 
 private:
     T real, img;
@@ -20,22 +23,28 @@ private:
 
 template <typename T>
 std::ostream& operator<<(std::ostream& os, Complex<T>& rhs) {
-    os << rhs.real << " " << rhs.img;
+    os << "real = " << rhs.real << ", "
+       << "img = " << rhs.img  << ", "
+       << "size = " << rhs.size();
     return os;
 }
 
 template <typename T>
-Complex<T>& operator*(Complex<T>& lhs, Complex<T>& rhs) {
-    T real = lhs.real * rhs.real - lhs.img * rhs.img;
-    T img = lhs.real * rhs.img + lhs.img * rhs.real;
-    lhs.real = real, lhs.img = img;
-    return lhs;
+Complex<T> operator*(Complex<T>& lhs, Complex<T>& rhs) {
+    return Complex<T>(static_cast<T>(lhs.real * rhs.real -
+                                     lhs.img * rhs.img),
+                      static_cast<T>(lhs.real * rhs.img +
+                                     lhs.img * rhs.real));
 }
 
 template <typename T>
 int mandelbrot_set(T x, T y) {
     Complex<T> point(x, y);
-    std::cout << point << std::endl;
-    std::cout << point * point << std::endl;
-    return 0;
+    int res = 0;
+    while (point.size() <= 4.0 && res < INF) {
+        point = point * point;
+        // std::cout << point << std::endl;
+        res++;
+    }
+    return res;
 }
